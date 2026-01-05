b0c990a8c977dea86c9269b81712be0de403b1ee
programmernusantara <pronus.programmernusantara@gmail.com>
2026-01-06 04:29:49 +0700

    deploy nusantara

--- lib/main.server.dart
+++ lib/main.server.dart
@@ -25,6 +25,7 @@ void main() {
   // [Document] renders the root document structure (<html>, <head> and <body>)
   // with the provided parameters and components.
   runApp(Document(
+    base: '/nusantara/',
     title: 'nusantara',
     styles: [
       // Special import rule to include to another css file.
